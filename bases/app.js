4b4036c16658dd2057cc7f070ef7705cbb496002
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2022-05-31 22:13:17 -0500

    JCMA Sección 1 Curso TS

---
+++ bases/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const mensaje = 'Hola Mundo';
+console.log(mensaje);
+const hero = {
+    name: 'Iron Man',
+    age: 45
+};
+// Error de tipo
+//hero.age = '60';
+console.log(hero.age);
---
+++ bases/app.ts
@@ -0,0 +1,13 @@
+const mensaje: string = 'Hola Mundo';
+
+console.log(mensaje);
+
+const hero = {
+    name: 'Iron Man',
+    age: 45
+};
+
+// Error de tipo
+//hero.age = '60';
+
+console.log(hero.age);
